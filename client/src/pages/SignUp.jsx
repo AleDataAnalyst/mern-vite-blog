1c0d42009448c956521393a52a967d54b1e3ba92
AleDataAnalyst <criptolmin@gmail.com>
2024-04-18 11:06:43 +0200

    cambio de nombre

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,10 +43,7 @@ export default function SignUp() {
         {/* left */}
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Aleng
-            </span>
-            Coder's Blog
+            CodeSprinkler
           </Link>
           <p className='text-sm mt-5'>
             Puedes registrarte con tu email y contraseña o con Google.
